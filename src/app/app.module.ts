2c3ff220a5c569def579dbd0c6d1e4649581ee95
Francky <gwadafrank@gmail.com>
2020-11-04 14:33:25 +0300

    monday and tuesday works
     - creating components
     - creating a class
     - output binding and input binding
     - emiting events
     - directive
     - pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoalComponent } from './goal/goal.component';
+import { GoalDetailComponent } from './goal-detail/goal-detail.component';
+import { StrikethroughDirective } from './strikethrough.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoalComponent,
+    GoalDetailComponent,
+    StrikethroughDirective
   ],
   imports: [
     BrowserModule,
