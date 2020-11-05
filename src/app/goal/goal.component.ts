7682b1127e3846444221cde6624d13dc95beb63d
Francky <gwadafrank@gmail.com>
2020-11-05 12:16:22 +0300

    Implement form component
     - ngModule 2 way binding
     - Form validation
     - Form submissison

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -36,6 +36,18 @@ export class GoalComponent implements OnInit {
       }
     }
   }
+
+  addNewGoal(goal:Goal){
+    console.log(goal);
+    
+    if (goal){
+      let goalLength = this.goals.length
+      goal.id = goalLength + 1
+      // goal.completeDate = new Date (goal.completeDate)
+      this.goals.push(goal)
+    }
+    
+  }
   
   constructor(){
     
